14a400015abf80186a6353ddb4fe1dfb1d84ed86
Elledub <laurentwaisman@gmail.com>
2026-03-17 17:51:29 +0100

    cutover: set site to asymmetricalcollage.com, remove base, enable CNAME

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
-// Astro configuration
-// TESTING: deployed at elledub-lw.github.io/asymmetrical-collage-astro
-// CUTOVER: remove base, set site to https://www.asymmetricalcollage.com
-
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://elledub-lw.github.io',
-  base: '/asymmetrical-collage-astro/',
+  site: 'https://www.asymmetricalcollage.com',
 });
